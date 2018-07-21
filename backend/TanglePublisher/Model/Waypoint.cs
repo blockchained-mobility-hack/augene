d203f2dc0fa7585ae48d57c351179e3d8d9e5a6d
Simon Pfeifhofer <simon.pfeifhofer@gmail.com>
2018-07-22 00:27:54 +0200

    remove single routes, adopt model, add recording-date

--- backend/TanglePublisher/Model/Waypoint.cs
+++ backend/TanglePublisher/Model/Waypoint.cs
@@ -8,14 +8,23 @@ namespace TanglePublisher.Model
     public class Waypoint
     {
 
-        [JsonProperty("lon")]
-        public decimal Lon { get; set; }
+        [JsonProperty("longitude")]
+        public decimal Longitude { get; set; }
 
-        [JsonProperty("lat")]
-        public decimal Lat { get; set; }
+        [JsonProperty("latitude")]
+        public decimal Latitude { get; set; }
 
-        [JsonProperty("speed")]
-        public int Speed { get; set; }
+        [JsonProperty("elevation")]
+        public int Elevation { get; set; }
+
+        [JsonProperty("battery_state_of_charge")]
+        public int BatteryStateOfCharge { get; set; }
+
+        [JsonProperty("outside_temp")]
+        public int OutsideTemp { get; set; }
+
+        [JsonProperty("driving_behavior")]
+        public int DrivingBehavior { get; set; }
 
     }
 }
