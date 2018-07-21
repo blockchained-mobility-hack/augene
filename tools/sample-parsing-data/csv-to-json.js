7af5fbc3a8486298779b7dfb0b97496297196f82
Gábor Kerekes <gbr@krks.info>
2018-07-21 20:27:15 +0200

    Update Mock Data

--- tools/sample-parsing-data/csv-to-json.js
+++ tools/sample-parsing-data/csv-to-json.js
@@ -1,35 +1,62 @@
 const parse = require("csv-parse/lib/sync");
 const fs = require("fs");
-const path = require("path")
+const path = require("path");
 
+const tripCount = 3;
 path.resolve(__dirname, "..", "..", "frontend", "public");
 
-const table = parse(
-  fs.readFileSync(path.resolve(__dirname, "data", "sample-trip.csv"), "utf8")
-);
-
-const [header, ...rows] = table;
-
-const data = rows.map((row, acc) => {
-  return row.reduce((acc, value, i) => {
-    // console.log("value", value);
-    const key = header[i];
-    if(key == ''){
-      return acc;
-    }
-    const camelcasedKey = key.toLowerCase().replace(/\s/g, "_");
-    return Object.assign(acc, {
-      [camelcasedKey]: parseFloat(value)
+const trips = new Array(tripCount)
+  .fill()
+  .map((_, i) => {
+    console.log(i);
+    const table = parse(
+      fs.readFileSync(
+        path.resolve(__dirname, "data", `sample-trip-${i}.csv`),
+        "utf8"
+      )
+    );
+    const [header, ...rows] = table;
+    return rows.map((row, acc) => {
+      return row.reduce((acc, value, i) => {
+        // console.log("value", value);
+        const key = header[i];
+        if (key == "") {
+          return acc;
+        }
+        const camelcasedKey = key.toLowerCase().replace(/\s/g, "_");
+        return Object.assign(acc, {
+          [camelcasedKey]: parseFloat(value)
+        });
+      }, {});
     });
-  }, {});
-});
-
+  })
+  .map(waypoints => {
+    return {
+      name: `Test-Route-${Math.random()}`,
+      from: "Berlin",
+      to: "Munich",
+      vehicle_type: "BMW i3",
+      consumption_percentage: 45,
+      waypoints
+    };
+  });
 
 const outDir = path.resolve(__dirname, "export");
-fs.writeFileSync(path.resolve(outDir, "sample-trip.json"), JSON.stringify(data));
+fs.writeFileSync(
+  path.resolve(outDir, `sample-trips.json`),
+  JSON.stringify(trips)
+);
 
-const dataTemplate = require("./model/datamodel.json");
-const [trip] = dataTemplate;
+const mockData = trips.map(data => {
+  return {
+    data,
+    hash: "",
+    proof_link: "https://thetangle.org"
+  }
+});
 
-trip.data.waypoints = data;
-fs.writeFileSync(path.resolve(__dirname, "..", "..", "frontend", "public", "mock-data.json"), JSON.stringify([trip]));
+// console.log(JSON.stringify(mockData, null, 4));
+fs.writeFileSync(
+  path.resolve(__dirname, "..", "..", "frontend", "public", `mock-data.json`),
+  JSON.stringify(mockData, null, 4)
+);
