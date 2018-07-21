5cb75aaecd4e5e0bd352336739c24defbc7da7cb
Boyd <boyd@rndr.studio>
2018-07-21 18:34:09 +0200

    highlight lines and rotation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,11 +17,14 @@ export const INITIAL_VIEW_STATE = {
   zoom: 8,
   maxZoom: 16,
   pitch: 50,
-  bearing: 0
+  bearing: 20
 };
 
 
+
+
 const { REACT_APP_MAPBOX_TOKEN } = process.env
+
 
 
 var dataDemo = [
@@ -44,8 +47,28 @@ export default class App extends Component {
     super(props);
     this.state = {
       remoteData: RemoteData.loading(),
-      hoveredObject: null
+      hoveredObject: null,
+      viewState: INITIAL_VIEW_STATE
     };
+    var t=setInterval(this._updateView,10);
+    console.log(props);
+  }
+
+  _updateView = () => {
+      this.setState({ viewState: {
+  latitude: 48.7665,
+  longitude: 11.4258,
+  zoom: 8,
+  maxZoom: 16,
+  pitch: 50,
+  bearing: this.state.viewState.bearing+0.1
+} 
+
+       })
+      
+
+     // console.log("update");
+
   }
 
   componentWillMount() {
@@ -57,25 +80,48 @@ export default class App extends Component {
   }
 
   _onHover = ({x, y, object}) => {
+    //console.log(object);
+      if(object == undefined) {
+        console.log('set null')
+           this.setState({hoveredObject: null});
+      } else {
+        this.setState({hoveredObject: {x, y, object}});
+      }
     
-     this.setState({hoveredObject: {x, y, object}});
   }
 
   _renderTooltip = () => {
     if(this.state.hoveredObject){
           const { x, y, object } = this.state.hoveredObject;
-console.log(x, y);
           return (
                 <div className="tooltip" style={{left: x, top: y}}>
-                  <div>testtestset</div>
+                  {object.to.name}
                 </div>
           )
     }
+
     return null;
+  }
+
+  _changeColor = () => {
+    if(this.state.hoveredObject) {
+      return [255, 0, 0, 255];
+    } else {
+      return [255, 0, 0, 155];
+    }
+  }
+
+  _changeStrokeWidth = () => {
+    if(this.state.hoveredObject) {
+      return 10;
+    } else {
+      return 2;
+    }
   }
   
   renderData = (data) => {
-    const { viewState } = this.props;
+    const { viewState } = this.state;
+   
 
     //console.log(data);
     const layers = [
@@ -83,14 +129,21 @@ console.log(x, y);
         id: 'line-layer',
         data: dataDemo,
         pickable: true,
-        getStrokeWidth: 12,
+        getStrokeWidth: this._changeStrokeWidth,
         getSourcePosition: d => {
-          console.log(d);
           return d.from.coordinates
         } ,
         getTargetPosition: d => d.to.coordinates,
-        getColor: d => [Math.sqrt(d.inbound + d.outbound), 140, 0],
-        onHover: this._onHover
+        getColor: this._changeColor,
+        onHover: this._onHover,
+        updateTriggers: {
+          getColor: {
+           value: this.state.hoveredObject
+          },
+          getStrokeWidth: {
+           value: this.state.hoveredObject
+          }
+        }
       })
       // new GeoJsonLayer({
       //   id: "geojson-layer",
@@ -114,6 +167,7 @@ console.log(x, y);
       <DeckGL
         layers={layers}
         initialViewState={INITIAL_VIEW_STATE}
+
         viewState={viewState}
         controller={true}
         pickingRadius={5}
